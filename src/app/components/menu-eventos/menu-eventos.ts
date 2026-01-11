6d0d9befd44c662dd7d9c465a9cb2319a9f9836a
raulgar19 <raulgamu2003@gmail.com>
2026-01-11 12:58:46 +0100

    menu-eventos component created

---
+++ src/app/components/menu-eventos/menu-eventos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-eventos',
+  imports: [],
+  templateUrl: './menu-eventos.html',
+  styleUrl: './menu-eventos.css',
+})
+export class MenuEventos {
+
+}
