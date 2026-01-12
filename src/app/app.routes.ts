b96b7d287a732b5b0d7e8a36ffdb31e4be71947b
xewi-11 <javi11coimbra@gmail.com>
2026-01-12 10:16:02 +0100

    feat: Refactor routing and remove unused header component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,14 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login';
 import { HomeComponent } from './components/header/header';
 import { authGuard } from './auth/auth-guard';
+import { MenuEventos } from './components/menu-eventos/menu-eventos';
+import { MenuActividades } from './components/menu-actividades/menu-actividades';
 
-export const routes: Routes = [
-    { path: 'login', component: LoginComponent },
 
-  {
-    path: 'home',
-    component: HomeComponent,
-    canActivate: [authGuard]
-  },
-   { path: '', redirectTo: 'home', pathMatch: 'full' }
+    export const routes: Routes = [
+    { path: '', component: LoginComponent },
+    {path: 'eventos', component: EventSource, canActivate: [authGuard]},
+    {path: 'detallesEvento', component: MenuEventos,canActivate: [authGuard]},
+    {path: 'detallesActividad', component: MenuActividades,canActivate: [authGuard]},
+    {path: 'perfil', component: PerformanceObserverEntryList,canActivate: [authGuard]},
 ];
