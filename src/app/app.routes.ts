e4260707fd35bbc53378571c400d9d834c5d5315
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2026-01-12 20:56:17 +0100

    Integración de router en el menu de actividades

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { LoginComponent } from './auth/login/login';
 import { authGuard } from './auth/auth-guard';
 import { MenuEventos } from './components/menu-eventos/menu-eventos';
 import { MenuActividades } from './components/menu-actividades/menu-actividades';
+import { PartidosActividad } from './components/partidos-actividad/partidos-actividad';
+import { MaterialesActividad } from './components/materiales-actividad/materiales-actividad';
+import { CapitanesActividad } from './components/capitanes-actividad/capitanes-actividad';
+import { EquiposActividad } from './components/equipos-actividad/equipos-actividad';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
@@ -10,5 +14,8 @@ export const routes: Routes = [
   { path: 'detallesEvento', component: MenuEventos, canActivate: [authGuard] },
   { path: 'detallesActividad', component: MenuActividades, canActivate: [authGuard] },
   { path: 'perfil', component: PerformanceObserverEntryList, canActivate: [authGuard] },
-  { path: 'detallesActividad', component: MenuActividades, canActivate: [authGuard] },
+  { path: 'partidosActividad', component: PartidosActividad, canActivate: [authGuard] },
+  { path: 'materialesActividad', component: MaterialesActividad, canActivate: [authGuard] },
+  { path: 'capitanesActividad', component: CapitanesActividad, canActivate: [authGuard] },
+  { path: 'equiposActividad', component: EquiposActividad, canActivate: [authGuard] },
 ];
