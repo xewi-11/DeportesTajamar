ad5830e54d6bd0c32711f0a963e17ff7bcc583ab
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2026-01-13 20:05:47 +0100

    finalización menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login';
 import { authGuard } from './auth/auth-guard';
 import { MenuEventos } from './components/menu-eventos/menu-eventos';
-import { MenuActividades } from './components/menu-actividades/menu-actividades';
 import { PartidosActividad } from './components/partidos-actividad/partidos-actividad';
 import { MaterialesActividad } from './components/materiales-actividad/materiales-actividad';
 import { CapitanesActividad } from './components/capitanes-actividad/capitanes-actividad';
@@ -12,7 +11,6 @@ export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'eventos', component: EventSource, canActivate: [authGuard] },
   { path: 'detallesEvento', component: MenuEventos, canActivate: [authGuard] },
-  { path: 'detallesActividad', component: PartidosActividad, canActivate: [authGuard] },
   { path: 'perfil', component: PerformanceObserverEntryList, canActivate: [authGuard] },
   { path: 'partidosActividad', component: PartidosActividad, canActivate: [authGuard] },
   { path: 'materialesActividad', component: MaterialesActividad, canActivate: [authGuard] },
